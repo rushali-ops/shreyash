c9ac2b1107304ce93d8786a01f62bd896cd86425
rushali-ops <76438984+rushali-ops@users.noreply.github.com>
2023-09-22 08:59:25 +0530

    Add files via upload

---
+++ shreyash/main.py
@@ -0,0 +1,45 @@
+import plotly.figure_factory as ff
+import statistics
+import csv
+import pandas as pd
+import random
+
+df=pd.read_csv("temperature.csv")
+data=df["temp"].tolist()
+
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+
+
+
+
+def show_fig(mean_list):
+    df = mean_list
+    mean = statistics.mean(df)
+    fig = ff.create_distplot([df], ["temp"], show_hist=False)
+    fig.show()
+    
+def setup():
+    mean_list = []
+    for i in range(0,1000):
+        set_of_means= random_set_of_mean(100)
+        mean_list.append(set_of_means)
+    show_fig(mean_list)
+    
+    mean = statistics.mean(mean_list)
+    print("Mean of sampling distribution :-",mean )
+
+setup()
+
+
+#Code to find the mean of the raw data ("population data")
+population_mean = statistics.mean(data)
+print("population mean:- ", population_mean)
